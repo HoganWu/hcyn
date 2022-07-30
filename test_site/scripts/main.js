3dfe17a1a25eebafb478709de41c17f5591573c8
wuhengcheng <wuhengcheng1@163.com>
2022-07-30 13:37:29 +0800

    fox(index.html) change context

--- test_site/scripts/main.js
+++ test_site/scripts/main.js
@@ -1,35 +1,33 @@
+let myImage = document.querySelector("img");
 
-let myImage = document.querySelector('img');
-
-myImage.onclick =function(){
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/图层 1 拷贝.png'){
-        myImage.setAttribute('src','images/beijing2.png');
-    }else{
-        myImage.setAttribute('src','images/图层 1 拷贝.png');
-    }
-}
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/图层 1 拷贝.png") {
+    myImage.setAttribute("src", "images/beijing2.png");
+  } else {
+    myImage.setAttribute("src", "images/图层 1 拷贝.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    if(!myName){
-        setUserName();
-    }else{
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
-    }
-  }
-
-if(!localStorage.getItem('name')) {
+  let myName = prompt("请输入你的名字。");
+  if (!myName) {
     setUserName();
   } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
   }
+}
 
-myButton.onclick = function() {
-    setUserName();
- }
- 
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
